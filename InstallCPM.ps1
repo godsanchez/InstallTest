bc414fe58bbd5e6a5d71d5c5c3c26ba6c2cb7659
Jose Meliton Sanchez Saldana <josemelitons@microsoft.com>
2026-02-11 09:52:36 -0800

    Added unzip and installation

--- InstallCPM.ps1
+++ InstallCPM.ps1
@@ -3,3 +3,9 @@ powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | ie
 
 # Download cpm package
 Invoke-WebRequest -Uri "https://github.com/godsanchez/InstallTest/archive/refs/heads/main.zip"-OutFile ./main.zip
+
+# Unzip the package
+Expand-Archive -Path ./main.zip -DestinationPath ./CPMInstall
+
+# Install the package using uv
+uv tool install ./CPMInstall/InstallTest-main/src/CPM
