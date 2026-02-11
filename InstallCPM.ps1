7336ba5c7d3fce9a5430ec7f207916493d26e5fd
Jose Meliton Sanchez Saldana <josemelitons@microsoft.com>
2026-02-11 09:49:49 -0800

    Added package download

--- InstallCPM.ps1
+++ InstallCPM.ps1
@@ -1,1 +1,5 @@
+# Install uv tool for python package management
 powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex" 
+
+# Download cpm package
+Invoke-WebRequest -Uri "https://github.com/godsanchez/InstallTest/archive/refs/heads/main.zip"-OutFile ./main.zip
