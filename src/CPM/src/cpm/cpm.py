23932f68c791a6a50ad290769195b593b3c2d206
Jose Meliton Sanchez Saldana <josemelitons@microsoft.com>
2026-02-11 09:39:33 -0800

    Initial CPM arguments

--- src/CPM/src/cpm/cpm.py
+++ src/CPM/src/cpm/cpm.py
@@ -3,21 +3,16 @@ from typing_extensions import Annotated
 
 
 def cpm(
-    name: Annotated[str, typer.Argument(help="The (last, if --title is given) name of the person to greet")] = "",
-    title: Annotated[str, typer.Option(help="The preferred title of the person to greet")] = "",
-    doctor: Annotated[bool, typer.Option(help="Whether the person is a doctor (MD or PhD)")] = False,
-    count: Annotated[int, typer.Option(help="Number of times to greet the person")] = 1
+    install: Annotated[bool, typer.Option(help="Install a CRC package.")] = False,
+    update: Annotated[bool, typer.Option(help="Update a CRC package, or all installed software.")] = False,
+    download: Annotated[bool, typer.Option(help="Download a CRC package (without installing).")] = False,
+    list: Annotated[bool, typer.Option(help="List installed/downloaded CRC packages.")] = False,
 ):
-    greeting = "Greetings, "
-    if doctor and not title:
-        title = "Dr."
-    if not name:
-        if title:
-            name = title.lower().rstrip(".")
-        else:
-            name = "friend"
-    if title:
-        greeting += f"{title} "
-    greeting += f"{name}!"
-    for i in range(0, count):
-        print(greeting)
+    if install:
+        print("Installing a CRC package...")
+    if update:
+        print("Updating a CRC package...")
+    if download:
+        print("Downloading a CRC package...")
+    if list:
+        print("Listing installed/downloaded CRC packages...")
