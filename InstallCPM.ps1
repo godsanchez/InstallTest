de7b30fb8c22256ba702e657a439ac8372dd7814
godsanchez <38116933+godsanchez@users.noreply.github.com>
2026-02-18 13:14:50 -0800

    Update CPM installation process in InstallCPM.ps1
    Replaced the installation method for CPM with a Python script download and updated the installation command.

--- InstallCPM.ps1
+++ InstallCPM.ps1
@@ -1,14 +1,11 @@
 # Install uv tool for python package management
 powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex" 
 
-# Update user path to point to uv
-$env:Path = "$HOME\.local\bin;$env:Path"
+# Download cross-platform Python download script
+Invoke-WebRequest -Uri "https://aka.ms/CPMDownload" -OutFile ./downloadcpm.py
 
 # Download cpm package
-Invoke-WebRequest -Uri "https://github.com/godsanchez/InstallTest/archive/refs/heads/main.zip"-OutFile ./main.zip
-
-# Unzip the package
-Expand-Archive -Path ./main.zip -DestinationPath ./CPMInstall  -ErrorAction SilentlyContinue
+uv run .\downloadcpm.py
 
 # Install the package using uv
-uv tool install ./CPMInstall/InstallTest-main/src/CPM
+uv tool install ./ganymedepackagemanager-0.1.0.tar.gz
