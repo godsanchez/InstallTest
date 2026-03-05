50ef8fd394e6a390a709896dc701accf78a84322
godsanchez <38116933+godsanchez@users.noreply.github.com>
2026-03-04 16:54:06 -0800

    Update BLOB_PREFIX and CONTAINER_NAME in script

--- installcpm.sh
+++ installcpm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Default Portal values
-BLOB_PREFIX="ganymedepackagemanager"
+BLOB_PREFIX="cpm/ganymedepackagemanager"
 DOWNLOAD_PATH="."
 STORAGE_ACCOUNT_URL="https://crcportalstoragedev.blob.core.windows.net"
-CONTAINER_NAME="cpm"
+CONTAINER_NAME="assets"
 
 # Install uv tool for python package management
 curl -LsSf https://astral.sh/uv/install.sh | sh
@@ -18,5 +18,8 @@ wget "https://aka.ms/CPMDownload" -O ./downloadblob.py
 # Download cpm package
 uv run ./downloadblob.py --blob-prefix "$BLOB_PREFIX" --download-path "$DOWNLOAD_PATH" --account-url "$STORAGE_ACCOUNT_URL" --container-name "$CONTAINER_NAME"
 
+# Find latest package in download path
+PACKAGE_PATH=$(ls -t "$DOWNLOAD_PATH"/ganymedepackagemanager-* | head -n 1)
+
 # Install the package using uv, with bytecode compilation for faster imports
-uv tool install --compile-bytecode "$DOWNLOAD_PATH"
+uv tool install --compile-bytecode "$PACKAGE_PATH"
