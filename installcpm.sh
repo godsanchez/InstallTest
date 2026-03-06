9e6677a969410caeb892309e3610cc88639f2640
godsanchez <38116933+godsanchez@users.noreply.github.com>
2026-03-05 20:11:01 -0800

    Update BLOB_PREFIX and BLOB_NAME for version 8

--- installcpm.sh
+++ installcpm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Default Portal values
-BLOB_PREFIX="44/4464b461-ed11-41f5-98b7-2f5c40b76c19/7/crcpackagemanager"
-BLOB_NAME="44/4464b461-ed11-41f5-98b7-2f5c40b76c19/7/crcpackagemanager-0.1.4.tar.gz"
+BLOB_PREFIX="44/4464b461-ed11-41f5-98b7-2f5c40b76c19/8/crcpackagemanager"
+BLOB_NAME="44/4464b461-ed11-41f5-98b7-2f5c40b76c19/8/crcpackagemanager-0.1.4.tar.gz"
 DOWNLOAD_PATH="."
 STORAGE_ACCOUNT_URL="https://crcportalstoragedev.blob.core.windows.net"
 CONTAINER_NAME="assets"
