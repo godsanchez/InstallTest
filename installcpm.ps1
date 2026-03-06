9e56bd47fe7c9d7a92c2646054c7179f40cbfec5
godsanchez <38116933+godsanchez@users.noreply.github.com>
2026-03-05 19:50:55 -0800

    Update BLOB_PREFIX to new version path

--- installcpm.ps1
+++ installcpm.ps1
@@ -1,5 +1,5 @@
 # Default Portal values
-$BLOB_PREFIX = "44/4464b461-ed11-41f5-98b7-2f5c40b76c19/6/crcpackagemanager"
+$BLOB_PREFIX = "44/4464b461-ed11-41f5-98b7-2f5c40b76c19/7/crcpackagemanager"
 $DOWNLOAD_PATH = "."
 $STORAGE_ACCOUNT_URL = "https://crcportalstoragedev.blob.core.windows.net"
 $CONTAINER_NAME = "assets"
