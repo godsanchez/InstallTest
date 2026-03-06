8c6116814f7f04a80f9e6a329dbbeae84a505c0a
godsanchez <38116933+godsanchez@users.noreply.github.com>
2026-03-05 19:58:04 -0800

    Fix variable assignment and update download command

--- installcpm.sh
+++ installcpm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Default Portal values
-$BLOB_PREFIX = "44/4464b461-ed11-41f5-98b7-2f5c40b76c19/7/crcpackagemanager"
+BLOB_PREFIX="44/4464b461-ed11-41f5-98b7-2f5c40b76c19/7/crcpackagemanager"
+BLOB_NAME="44/4464b461-ed11-41f5-98b7-2f5c40b76c19/7/crcpackagemanager-0.1.4.tar.gz"
 DOWNLOAD_PATH="."
 STORAGE_ACCOUNT_URL="https://crcportalstoragedev.blob.core.windows.net"
 CONTAINER_NAME="assets"
@@ -16,7 +17,7 @@ export PATH="$HOME/.local/bin:$PATH"
 wget "https://aka.ms/CPMDownload" -O ./downloadblob.py
 
 # Download cpm package
-uv run ./downloadblob.py --blob-prefix "$BLOB_PREFIX" --download-path "$DOWNLOAD_PATH" --account-url "$STORAGE_ACCOUNT_URL" --container-name "$CONTAINER_NAME"
+uv run ./downloadblob.py --blob-name "$BLOB_NAME" --download-path "$DOWNLOAD_PATH" --account-url "$STORAGE_ACCOUNT_URL" --container-name "$CONTAINER_NAME"
 
 # Find latest package in download path
 PACKAGE_PATH=$(ls -t "$DOWNLOAD_PATH"/crcpackagemanager-* | head -n 1)
